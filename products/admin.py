329c25541321694f981d39f6e17092ab31bb4cf1
Mohamadamin <moaminalizadeh21@gmail.com>
2024-12-23 13:57:17 +0330

    update some templates

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'date_created', 'active', ]
+
--- products/models.py
+++ products/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 from django.shortcuts import reverse
 
+from django.contrib.auth import get_user_model
+
+
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
@@ -16,3 +19,28 @@ class Product(models.Model):
     
     def get_absolute_url(self):
         return reverse("product_detail", args=[self.pk])
+
+
+
+class Comment(models.Model):
+    PRODUCT_POINTS = [
+        ('1', 'Very Bad!'),
+        ('2', 'Bad'),
+        ('3', 'Not Bad'),
+        ('4', 'Good'),
+        ('5', 'Perfect!'),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    points = models.CharField(max_length=10, choices=PRODUCT_POINTS)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date_modified']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.author)
