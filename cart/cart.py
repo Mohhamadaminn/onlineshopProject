5fe25d095b272b9bbf54e7330c656593768e95ca
Mohhamadaminn <mohamad4aminm@gmail.com>
2025-11-17 11:29:26 +0330

    start creating cart view and template

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,6 @@
+from products.models import Product
+
+
 class Cart:
     def __init__(self, request):
         """
@@ -37,7 +40,7 @@ class Cart:
         product_id = str(product.id)
 
         if product_id in self.cart:
-            del self.cart(product_id)
+            del self.cart[product_id]
             self.save()
 
 
@@ -46,3 +49,33 @@ class Cart:
         Mark session as modified to save changes
         """       
         self.session.modified = True
+
+    
+    def __iter__(self):
+        product_ids = self.cart.keys()
+
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product_obj'] = product
+
+        for item in cart.values():
+            yield item
+
+    
+    def __len__(self):
+        return len(self.cart.keys())
+    
+
+    def clear(self):
+        del self.session['cart']
+        self.save()
+
+    
+    def get_total_price(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        return sum(product.price for product in products)
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .cart import Cart
 
-# Create your views here.
+
+def cart_detail_view(request):
+    cart = Cart(request)
+    return render(request, 'cart/cart_detail.html', {
+        'cart': cart
+    })    
